14c4eb6a75fa3d545fda522f8b2294e8631206a5
javier <feizaguirre@birt.eus>
2022-03-20 20:05:21 +0100

    Predefinir la pagina principal
    El scroll infinito se ha originado a partir de una función creada en local para comprobar su comportamiento.
    Todas las empresas tienen el mismo logo por simplicidad para realizar una prueba.
    Por lo demás todas ellas incorporan en el sliding las opciones de edición y borrado.
    Además se ha controlado que el checkbox para seleccionar las empresas a representar en el gráfico, esté activo mientras no se hayan seleccionado tres de ellas.
    En ese momento se inhabilitarán el resto, y se activará el botón de mostrar gráfico.
    El acabado del diseño se harán al final.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,79 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  constructor() {}
+  items = [];
+  toggle = "Detener scroll infinito";
+  cargas = 4;
+  contador = 1;
+
+  public checkeds = 0;
+  public limit = 3;
+
+  constructor(public alertController: AlertController) {
+    this.addMoreItems(this.contador);
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      this.addMoreItems(this.contador);
+      this.cargas -= 1;
+      console.log(this.cargas);
+      event.target.complete();
+    }, 1000);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+    if(this.toggle == "Detener scroll infinito"){
+      this.toggle = "Activar scroll infinito";
+    } else {this.toggle = "Detener scroll infinito"}
+  }
+
+  comprobar(seleccionado) {
+    if (!seleccionado.isChecked){
+      this.checkeds++;
+      console.log(this.checkeds);
+    } else {
+      this.checkeds--;
+      console.log(this.checkeds);
+    }
+  }
+
+  addMoreItems(contador: number){
+    for(let i = contador; i < contador + 20; i++){
+      this.items.push({nombre: 'Empresa ' + i, isChecked: false});
+    }
+    this.contador += 20;
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirmar!',
+      message: '¿Esta seguro que quiere eliminar la <strong>empresa</strong>?!!!',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Operacion cancelada');
+          }
+        }, {
+          text: 'Eliminar',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirmar Eliminar Empresa');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
