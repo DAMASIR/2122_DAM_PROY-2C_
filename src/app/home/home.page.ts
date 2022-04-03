5168d6e2a810552251c0152bed85eea5899403b7
javier <feizaguirre@birt.eus>
2022-04-03 17:02:06 +0200

    Definir scroll infinito de pagina principal
    Se define el scroll infinito del listado de empresas desde llamadas paginadas al servidor.
    
    El total de cargas o numero de paginas a cargar está definido manualmente.
    
    Todas las empresas listadas se guardan en un array en el servicio de empresas, para poder controlar el listado tras las distintas operaciones CRUD.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonInfiniteScroll } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
+import { ServicioEmpresaService } from '../services/servicio-empresa.service';
+import { HttpServicioService } from '../services/http-servicio.service';
+import { Empresa } from '../models/empresa';
 
 @Component({
   selector: 'app-home',
@@ -10,28 +13,29 @@ import { AlertController } from '@ionic/angular';
 export class HomePage {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  items = [];
   toggle = "Detener scroll infinito";
-  cargas = 4;
-  contador = 1;
+  cargas = 3;
+  url: string;
+  pagina = 1;
+  empresas_por_pagina = 8;
 
   public checkeds = 0;
   public limit = 3;
 
-  constructor(public alertController: AlertController) {
-    this.addMoreItems(this.contador);
+  constructor(public servicio: ServicioEmpresaService , public httpServicio: HttpServicioService, public alertController: AlertController) {
+    this.getEmpresas(false, "");
   }
 
+  // Metodo llamado por el scroll infinito cuando se activa
   loadData(event) {
     setTimeout(() => {
       console.log('Done');
-      this.addMoreItems(this.contador);
-      this.cargas -= 1;
-      console.log(this.cargas);
-      event.target.complete();
+      this.getEmpresas(true, event);
+      this.cargas--;
     }, 1000);
   }
 
+  // Metodo para activar o desactivar el scroll infinito
   toggleInfiniteScroll() {
     this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
     if(this.toggle == "Detener scroll infinito"){
@@ -39,6 +43,7 @@ export class HomePage {
     } else {this.toggle = "Detener scroll infinito"}
   }
 
+  // Metodo utilizado al seleccionar las empresas que se utilizaran en los graficos
   comprobar(seleccionado) {
     if (!seleccionado.isChecked){
       this.checkeds++;
@@ -49,14 +54,28 @@ export class HomePage {
     }
   }
 
-  addMoreItems(contador: number){
-    for(let i = contador; i < contador + 20; i++){
-      this.items.push({nombre: 'Empresa ' + i, isChecked: false});
-    }
-    this.contador += 20;
+  // Metodo para recargar empresas en el scroll infinito
+  getEmpresas(otraCarga, event) {
+
+    this.url = '?_page=' + this.pagina + '&_limit=' + this.empresas_por_pagina;
+
+    this.httpServicio.getEmpresasList(this.url)
+      .subscribe((data: any) => {
+        for (let i = 0; i < data.length; i++) {
+          this.servicio.empresas.push(data[i]);
+        }
+        console.log(this.servicio.empresas);
+        if (otraCarga)
+          event.target.complete();
+
+        this.pagina++;
+      }, error => {
+        console.log(error);
+      })
   }
 
-  async presentAlertConfirm() {
+  // Metodo para manejar la eliminacion de una empresa a través de una ventana de alerta para evitar eliminaciones indeseadas
+  async presentAlertConfirm(id) {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Confirmar!',
@@ -74,12 +93,13 @@ export class HomePage {
           text: 'Eliminar',
           id: 'confirm-button',
           handler: () => {
+
             console.log('Confirmar Eliminar Empresa');
+            console.log(id);
           }
         }
       ]
     });
-
     await alert.present();
   }
 
